55306f128a2769de3332b26fbe9e333c1d7b0bdf
Alex Fridlyand <alex.fridlyand@temzix.com>
2018-02-18 21:24:51 +0300

    add i3 configs

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,8 @@ set -g theme_display_k8s_context no
 set -g theme_newline_cursor yes
 set -g theme_color_scheme base16-light
 
+set -gx TERMINAL urxvt
+
 # start X at login
 if status --is-login
     if test -z "$DISPLAY" -a $XDG_VTNR = 1
