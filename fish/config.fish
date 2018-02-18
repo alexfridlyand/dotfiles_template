4ee108395446071c6a79caae47be9717abb619b4
Alex Fridlyand <alex.fridlyand@temzix.com>
2018-02-18 18:14:46 +0300

    add startup conf for X

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,10 @@
 set -g theme_display_k8s_context no
 set -g theme_newline_cursor yes
 set -g theme_color_scheme base16-light
+
+# start X at login
+if status --is-login
+    if test -z "$DISPLAY" -a $XDG_VTNR = 1
+        exec startx
+    end
+end
