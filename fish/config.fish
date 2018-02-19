62bd678b406e7934892cbfc133ec7212b0dd4867
Alex Fridlyand <alex.fridlyand@temzix.com>
2018-02-19 11:20:40 +0300

    add snaps + i3desktop configs

--- fish/config.fish
+++ fish/config.fish
@@ -4,6 +4,8 @@ set -g theme_color_scheme base16-light
 
 set -gx TERMINAL urxvt
 
+set -gx XDG_DATA_DIRS /var/lib/snapd/desktop
+
 # start X at login
 if status --is-login
     if test -z "$DISPLAY" -a $XDG_VTNR = 1
---
+++ install-snaps.sh
@@ -0,0 +1,4 @@
+sudo apt install snapd
+sudo snap login
+snap install skype --classic
+snap install slack --classic
