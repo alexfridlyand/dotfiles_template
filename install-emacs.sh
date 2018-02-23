e557210d460059a96f8072deea6d6053795eb083
Alex Fridlyand <alex.fridlyand@temzix.com>
2018-02-23 21:04:13 +0300

    add make install for emacs

--- install-emacs.sh
+++ install-emacs.sh
@@ -2,3 +2,6 @@ sudo apt install -y emacs
 
 git clone git@github.com:hlissner/doom-emacs.git -b develop ~/.emacs.d
 git clone git@github.com:alexfridlyand/doom-emacs-private.git ~/.config/doom
+
+cd ~/.emacs.d
+make install
