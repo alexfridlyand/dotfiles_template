31b4f8070b9c7a88c7a4fd85ec90a74be83513d9
Alex Fridlyand <alex.fridlyand@temzix.com>
2018-02-23 22:14:39 +0300

    add aliases

---
+++ fish/aliases.fish
@@ -0,0 +1,18 @@
+alias sc systemctl
+alias jc journalctl
+alias ssc 'sudo systemctl'
+
+alias emacs 'emacs -nw'
+
+alias reboot 'sudo systemctl reboot'
+alias shutdown 'sudo systemctl poweroff'
+
+alias q exit
+alias clr clear
+alias l ls
+alias ll 'ls -la'
+
+alias .. 'cd ..'
+alias ... 'cd ../..'
+alias .... 'cd ../../..'
+alias ..... 'cd ../../../..'
--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,5 @@
+. ~/.config/fish/aliases.fish
+
 set -g theme_display_k8s_context no
 set -g theme_newline_cursor yes
 set -g theme_color_scheme base16-light
