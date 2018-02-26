e7b59ffbb819a5faf7430e0a07aaef7b24a04d1b
Alex Fridlyand <alex.fridlyand@temzix.com>
2018-02-26 14:37:36 +0300

    change styles

--- fish/config.fish
+++ fish/config.fish
@@ -2,7 +2,7 @@
 
 set -g theme_display_k8s_context no
 set -g theme_newline_cursor yes
-set -g theme_color_scheme base16-light
+set -g theme_color_scheme solarized-light
 
 set -gx TERMINAL urxvt
 set -gx EDITOR "emacsclient"
