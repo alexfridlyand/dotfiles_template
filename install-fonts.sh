056fee640fe032ead8ddf4b96975b7d54b20e6fc
Alex Fridlyand <alex.fridlyand@temzix.com>
2018-02-26 16:42:57 +0300

    make fonts great again

--- install-fonts.sh
+++ install-fonts.sh
@@ -1,8 +1,2 @@
-# clone
-git clone https://github.com/powerline/fonts.git --depth=1
-# install
-cd fonts
-./install.sh
-# clean-up a bit
-cd ..
-rm -rf fonts
+git clone git@github.com:alexfridlyand/personal-fonts.git fonts
+cp -r fonts/* ~/.local/share/fonts/
