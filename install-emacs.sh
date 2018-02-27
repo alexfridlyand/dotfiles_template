5235da7f1d005749c3cd272cee9bda4a11c959b3
Alex Fridlyand <alex.fridlyand@temzix.com>
2018-02-27 10:13:06 +0300

    major refactoring

--- install-emacs.sh
+++ install-emacs.sh
@@ -1,9 +1,3 @@
 sudo apt install -y emacs
 
-git clone git@github.com:hlissner/doom-emacs.git -b develop ~/.emacs.d
-git clone git@github.com:alexfridlyand/doom-emacs-private.git ~/.config/doom
-
-ln -s ~/dotfiles/emacs/init.el ~/.emacs.d/init.el
-
-cd ~/.emacs.d
-make install
+git clone git@github.com:alexfridlyand/.emacs.d.git ~/.emacs.d
