d2777fcc268bd29498c1ce59e78647f81a846e5e
Alex Fridlyand <alex.fridlyand@temzix.com>
2018-03-01 11:40:16 +0300

    still volume fix in wrong place

--- fish/config.fish
+++ fish/config.fish
@@ -13,11 +13,3 @@ set -gx VISUAL "$EDITOR"
 
 set -gx XDG_DATA_DIRS /var/lib/snapd/desktop /usr/share/gnome
 set -gx XDG_CONFIG_HOME ~/.config
-
-# start X at login
-if status --is-login
-    if test -z "$DISPLAY" -a $XDG_VTNR = 1
-        exec startx
-    end
-    bash ~/.config/fish/volume.sh
-end
