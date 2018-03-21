2cdf2a1dc44be17436ef776d384504ccc2f7ac0d
Alex Fridlyand <alex.fridlyand@temzix.com>
2018-03-21 22:02:43 +0300

    m

--- fish/config.fish
+++ fish/config.fish
@@ -9,6 +9,7 @@ set -g theme_color_scheme dark
 #. ~/.config/omf/color_scheme.fish
 
 set -g theme_display_k8s_context no
+set -g theme_display_git_master_branch yes
 set -g theme_show_exit_status yes
 set -g theme_newline_cursor yes
 set -g fish_prompt_pwd_dir_length 0
